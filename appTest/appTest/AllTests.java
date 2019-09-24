bca98c3503bd2fb530036344e1ea196bf010cf14
shruthithangamma <shruthike@ymail.com>
2019-09-24 11:48:18 +0530

    Update AllTests.java
    Message day Test, text length test

--- appTest/appTest/AllTests.java
+++ appTest/appTest/AllTests.java
@@ -5,8 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MessageAppDayTest.class, MessageAppTest.class,
-		MessageAppTextareaLengthTest.class })
+@SuiteClasses({MessageAppDayTest.class, MessageAppTest.class,
+		MessageAppTextareaLengthTest.class})
 public class AllTests {
 
 }
